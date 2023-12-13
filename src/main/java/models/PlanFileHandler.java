645e72bbd4e73fb3621767b1f0a5e469515c7369
Shady Nessim <shadysdevs@gmail.com>
2023-12-13 23:30:36 +0200

    implemented PlanFileHandler and converted from json using regex to avoid splitting answers and sections array by comma

---
+++ src/main/java/models/PlanFileHandler.java
@@ -0,0 +1,79 @@
+package models;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PlanFileHandler extends FileHandler<Plan>{
+    private static final String FILE_PATH = "src/main/java/models/plans.json";
+    public PlanFileHandler() {
+        super(FILE_PATH);
+    }
+
+    @Override
+    protected String convertObjectToJson(Plan plan) {
+        return String.format(
+                "{\"username\":\"%s\"," +
+                        "\"name\":\"%s\"," +
+                        "\"id\":\"%s\"," +
+                        "\"answers\":%s," +
+                        "\"sectionsContent\":%s}",
+                plan.getUsername(), plan.getName(), plan.getId(),
+                plan.getAnswersString(), plan.getSectionsContentString());
+    }
+
+    @Override
+    protected Plan convertJsonToObject(String json) {
+        // parse json with regex
+        // {"username":"salma","name":"plan1","id":"1","answers":["answer 1","answer 2","answer 3","answer 4","answer 5","answer 6","answer 7"],"sectionsContent":["section 1","section 2","section 3","section 4","section 5","section 6","section 7"]}
+        // Define regex patterns for extracting values
+        Pattern usernamePattern = Pattern.compile("\"username\":\"([^\"]*)\"");
+        Pattern namePattern = Pattern.compile("\"name\":\"([^\"]*)\"");
+        Pattern idPattern = Pattern.compile("\"id\":\"([^\"]*)\"");
+        Pattern answersPattern = Pattern.compile("\"answers\":\\[([^\\]]*)\\]");
+        Pattern sectionsContentPattern = Pattern.compile("\"sectionsContent\":\\[([^\\]]*)\\]");
+
+        // Match patterns against the JSON string
+        Matcher usernameMatcher = usernamePattern.matcher(json);
+        Matcher nameMatcher = namePattern.matcher(json);
+        Matcher idMatcher = idPattern.matcher(json);
+        Matcher answersMatcher = answersPattern.matcher(json);
+        Matcher sectionsContentMatcher = sectionsContentPattern.matcher(json);
+
+        String username = "";
+        if (usernameMatcher.find()) {
+            username = usernameMatcher.group(1);
+        }
+
+        String name = "";
+        if (nameMatcher.find()) {
+            name = nameMatcher.group(1);
+        }
+
+        int id = 0;
+        if (idMatcher.find()) {
+            id = Integer.parseInt(idMatcher.group(1));
+        }
+
+        String[] answers = new String[7];
+        if (answersMatcher.find()) {
+            String answersString = answersMatcher.group(1);
+            String[] answersArray = answersString.split(",");
+            // remove quotes from each answer
+            for (int i = 0; i < answersArray.length; i++) {
+                answers[i] = answersArray[i].replace("\"", "");
+            }
+        }
+
+        String[] sectionsContent = new String[7];
+        if (sectionsContentMatcher.find()) {
+            String sectionsContentString = sectionsContentMatcher.group(1);
+            String[] sectionsContentArray = sectionsContentString.split(",");
+            // remove quotes from each section content
+            for (int i = 0; i < sectionsContentArray.length; i++) {
+                sectionsContent[i] = sectionsContentArray[i].replace("\"", "");
+            }
+        }
+
+        return new Plan(username, name, id, answers, sectionsContent);
+    }
+}
--- src/main/java/models/TestFiles.java
+++ src/main/java/models/TestFiles.java
@@ -16,5 +16,22 @@ public class TestFiles {
         // get users from file
         List<User> userList = userFileHandler.getObjects();
         System.out.println(userList);
+
+        // create dummy business plans
+        Plan plan1 = new Plan("salma", "plan1", 1, new String[]{"answer 1", "answer 2", "answer 3", "answer 4", "answer 5", "answer 6", "answer 7"}, new String[]{"section 1", "section 2", "section 3", "section 4", "section 5", "section 6", "section 7"});
+        Plan plan2 = new Plan("shady", "plan2", 2, new String[]{"answer 1", "answer 2", "answer 3", "answer 4", "answer 5", "answer 6", "answer 7"}, new String[]{"section 1", "section 2", "section 3", "section 4", "section 5", "section 6", "section 7"});
+        Plan plan3 = new Plan("salma", "plan3", 3, new String[]{"answer 1", "answer 2", "answer 3", "answer 4", "answer 5", "answer 6", "answer 7"}, new String[]{"section 1", "section 2", "section 3", "section 4", "section 5", "section 6", "section 7"});
+
+        // add plans to file
+        PlanFileHandler planFileHandler = new PlanFileHandler();
+        planFileHandler.addObject(plan1);
+        planFileHandler.addObject(plan2);
+        planFileHandler.addObject(plan3);
+
+        // get plans from file
+        List<Plan> planList = planFileHandler.getObjects();
+        for (Plan plan : planList) {
+            System.out.println(plan);
+        }
     }
 }
